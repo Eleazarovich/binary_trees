1654ead3268c057b3d552f548e5b094c17cc93a3
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-03-28 12:35:07 +0200

    minor fixes [is root]

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,6 +3,9 @@
 /**
  * binary_tree_is_root - checks if a given node is a root
  * @node: pointer to the node to check
+ *
+ * Return: return 1 if node is a root, otherwise 0 and if node is NULL
+ *
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
@@ -11,5 +14,5 @@ int binary_tree_is_root(const binary_tree_t *node)
 
 	if (node->parent == NULL)
 		return (1);
-	return(0);
+	return (0);
 }
